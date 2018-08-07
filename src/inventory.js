5987f590ca5d1aacfbdcfd20a126a7ac9791e71c
Matthew Freeman <matthew.freeman821@gmail.com>
2018-08-07 14:31:50 -0400

    Added inventory component

---
+++ src/inventory.js
@@ -0,0 +1,28 @@
+import React from 'react';
+// import {Link} from 'react-router-dom';
+import NavBar from './navBar';
+
+class Inventory extends React.Component {
+    render() {
+        return (
+            <div className='inventory'>
+                <div className='navBar'>
+                    <NavBar />
+                </div>
+                <div className='invenOverview'>
+                    <h2>Inventory</h2>
+                    <p>Real time look at areas, sections, and bottles</p>
+                    <input type="button" className="btn" value="Add Area"/>
+                    <input type="button" className="btn" value="Run Inventory"/>
+                    <div className='areaOverview'>
+                        <div className='area'>
+                            <p>This is first area</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Inventory;
