162d9e370fbc290c8377ebc2c580deb96abb062b
Matthew Freeman <matthew.freeman821@gmail.com>
2018-08-07 12:48:33 -0400

    Added sign up component

---
+++ src/signup.js
@@ -0,0 +1,39 @@
+import React from 'react';
+// import {Link} from 'react-router-dom';
+
+class SignUp extends React.Component {
+    render() {
+        return (
+            <div className='signUpForm'>
+                <h1>BevSimply</h1>
+                <p>Sign-Up</p>
+                <form>
+                    <label>
+                        First Name:
+                        <input type="text" name="firstName" />
+                    </label>
+                    <label>
+                        Last Name:
+                        <input type="text" name="lastName" />
+                    </label>
+                    <label>
+                        Phone Number:
+                        <input type="text" name="phoneNumber" />
+                    </label>
+                    <label>
+                        Email:
+                        <input type="text" name="email" />
+                    </label>
+                    <label>
+                        Password:
+                        <input type="text" name="password" />
+                    </label>
+                    <input type="submit" value="Next" />
+
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SignUp;
