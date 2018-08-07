cc8a5275a65bdf7f8fe182d8690f09ed7d72a271
Matthew Freeman <matthew.freeman821@gmail.com>
2018-08-07 16:03:00 -0400

    Added personnel component

---
+++ src/personnel.js
@@ -0,0 +1,28 @@
+import React from 'react';
+// import {Link} from 'react-router-dom';
+import NavBar from './navBar';
+
+class Personnel extends React.Component {
+    render() {
+        return (
+            <div className='personnelPage'>
+                <div className='navBar'>
+                    <NavBar />
+                </div>
+                <div className='overview'>
+                    <h2>Personnel</h2>
+                    <p>Employees with access to your organization</p>
+                    <div className='inviteUser'>
+                        <input type="button" className="btn" value="Invite User"/>
+                    </div>
+                    <div className='personnelOverview'>
+                        <p>Employee Name</p>
+                        <p>Email</p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Personnel;
