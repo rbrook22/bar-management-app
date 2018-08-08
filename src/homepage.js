8f6376e351c75f8717a58ef5e9e9803adc6ed745
Richard Brooks <rickbrooks22@gmail.com>
2018-08-08 07:03:30 -0400

    added main div container on homepage

--- src/homepage.js
+++ src/homepage.js
@@ -4,12 +4,14 @@ import { Link } from 'react-router-dom';
 class HomePage extends React.Component {
     render() {
         return (
-            <div className='Home'>
-                <h1>BevSimply</h1>
-            </div>
-            <div className='logins'>
-                <Link to='/signup'><p>Sign up</p></Link>
-                <Link to='/signin'><p>Sign in</p></Link>
+            <div>
+                <div className='Home'>
+                    <h1>BevSimply</h1>
+                </div>
+                <div className='logins'>
+                    <Link to='/signup'><p>Sign up</p></Link>
+                    <Link to='/signin'><p>Sign in</p></Link>
+                </div>
             </div>
         );
     }
