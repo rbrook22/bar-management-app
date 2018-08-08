33f02204fc5b0f8b1d11fcd9a0ee27176fd0f88d
Richard Brooks <rickbrooks22@gmail.com>
2018-08-08 07:01:54 -0400

    added link tags to homepage

--- src/homepage.js
+++ src/homepage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class HomePage extends React.Component {
     render() {
@@ -7,10 +7,10 @@ class HomePage extends React.Component {
             <div className='Home'>
                 <h1>BevSimply</h1>
             </div>
-            // <div className='logins'>
-            //     <Link to='/signup'><p>Sign up</p></Link>
-            //     <Link to='/signin'><p>Sign in</p></Link>
-            // </div>
+            <div className='logins'>
+                <Link to='/signup'><p>Sign up</p></Link>
+                <Link to='/signin'><p>Sign in</p></Link>
+            </div>
         );
     }
 }
