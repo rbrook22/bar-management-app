cd70adf3c27fb39d74a4af8089df659656e24b02
Richard Brooks <rickbrooks22@gmail.com>
2018-08-09 13:38:32 -0400

    changed route names and filenames

---
+++ src/venueSettings.js
@@ -0,0 +1,33 @@
+import React from 'react';
+// import {Link} from 'react-router-dom';
+import NavBar from './navBar';
+
+class VenueSettings extends React.Component {
+    render() {
+        return (
+            <div className='venuePage'>
+                <div className='navBar'>
+                    <NavBar />
+                </div>
+                <div className='overview'>
+                    <h2>Settings</h2>
+                    <div className='venueInfo'>
+                        <div className='accountInfo'>
+                            <div className='accInfo'>
+                            <p>Account information here</p>
+                            </div>
+                            <div className='establishmentInformation'>
+                            <p>Establishment information here</p>
+                            </div>
+                        </div>
+                            <div className='subscriptionInformation'>
+                            <p>Subscription information here</p>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default VenueSettings;
