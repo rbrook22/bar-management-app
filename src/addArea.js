1e6478379f0f50dd3380c6d404545514e90779ac
Richard Brooks <rickbrooks22@gmail.com>
2018-08-10 10:41:26 -0400

    changed link path on confirm button to addArea page

--- src/addArea.js
+++ src/addArea.js
@@ -1,7 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 class AddArea extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            form: {
+                label: ""
+            }
+        }
+    }
+    componentDidMount(){
+        this.setState({
+            baseForm: this.state.form
+        })
+    }
+
+    handleareaName=(event) => {
+        this.setState({
+            form:{
+                ...this.state.form,
+                label: event.target.value
+            }
+        })
+    }
+
+    handleEntry(event){
+        event.preventDefault();
+        let newArea = {
+            label: this.state.form.label
+        }
+
+        axios.post('http://localhost:3000')
+            .then(res => {
+                console.log(res);
+            })
+
+    }
     render() {
         return (
             <div className='addArea'>
@@ -12,7 +48,7 @@ class AddArea extends React.Component {
                         Area:
                         <input type="text" name="areaName" />
                     </label>
-                    <Link to='/area'><input type="submit" value="Confirm" /></Link>
+                    <Link to='/venue'><input type="submit" value="Confirm" /></Link>
                 </form>
             </div>
         );
