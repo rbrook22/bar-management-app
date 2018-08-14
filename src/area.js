fa50d396b1c250329fd0e364e7e3b9f2092b9e33
Matthew Freeman <matthew.freeman821@gmail.com>
2018-08-14 11:34:53 -0400

    Added number inputs for full bottles and partial amount on area page

--- src/area.js
+++ src/area.js
@@ -86,6 +86,8 @@ class AreaPage extends React.Component {
                                     return (
                                         <div key={placement.id}>
                                             {placement.label}
+                                            <input type="number" placeholder="Full Bottles" step="1" />
+                                            <input type="number" placeholder="Partial Amount" step=".1" />
                                             <input type="button" onClick={this.deletePlacement} value={placement.id}/>
                                         </div>)
                                     }else {
