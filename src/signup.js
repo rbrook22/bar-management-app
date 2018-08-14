4f090a2ca83ef95a3aca8fdbf816744cc21ac268
Richard Brooks <rickbrooks22@gmail.com>
2018-08-14 09:42:22 -0400

    created LocationSearch component, made dropdown list show on search for sign-up page

--- src/signup.js
+++ src/signup.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import LocationSearchInput from './locationSearchInput';
+
 
 class SignUp extends React.Component {
     render() {
@@ -28,6 +30,14 @@ class SignUp extends React.Component {
                         Password:
                         <input type="text" name="password" />
                     </label>
+                    <label>
+                        Venue Search:
+                        <LocationSearchInput />
+                    </label>
+                    <label>
+                        Position/Title:
+                        <input type="text"/>
+                    </label>
                     <Link to><input type="submit" value="Next" /></Link>
 
                 </form>
@@ -35,5 +45,5 @@ class SignUp extends React.Component {
         );
     }
 }
-
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBjdRMVQV_zyck-PvktB1kVUU1ZIQBCjlY&libraries=places"></script>
 export default SignUp;
