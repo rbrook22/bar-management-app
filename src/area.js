e7a5366176d653bcdad7445bc8f9e1898dcabc0a
Matthew Freeman <matthew.freeman821@gmail.com>
2018-08-16 09:52:45 -0400

    Updated to remove dummy placeholders

--- src/area.js
+++ src/area.js
@@ -89,7 +89,7 @@ class AreaPage extends React.Component {
                                             <img src={placement.img} alt=""/>
                                             {placement.label}
                                             <Link to={`/section/placement/${placement.id}`} style={{ textDecoration: 'none' }}><p>Update Inventory</p></Link>
-                                            <input type="button" onClick={this.deletePlacement} value={placement.id}/>
+                                            <input type="button" onClick={this.deletePlacement} value={placement.id} placeholder="Delete"/>
                                         </div>)
                                     }else {
                                         return null;
